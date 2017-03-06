712e13a8f60602f4be52f777bf7d14af12557db4
Ranger Tsao <cao.zhifu@gmail.com>
2017-03-06 21:36:45 +0800

    proxy web socket

--- vertx-proxy/src/main/java/io/shike/vertx/proxy/Proxy.java
+++ vertx-proxy/src/main/java/io/shike/vertx/proxy/Proxy.java
@@ -6,6 +6,8 @@ import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.http.HttpClientRequest;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.ServerWebSocket;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 
@@ -23,29 +25,44 @@ public class Proxy extends AbstractVerticle {
     public void start(Future<Void> startFuture) throws Exception {
         client = vertx.createHttpClient(new HttpClientOptions());
         server = vertx.createHttpServer()
-                      .websocketHandler(ws -> ws.handler(ws::writeBinaryMessage))
-                      .requestHandler(request -> {
-                          logger.info("proxying request:" + request.uri());
-                          HttpClientRequest proxyRequest = client.request(request.method(),
-                                                                          8080,
-                                                                          "localhost",
-                                                                          request.uri(),
-                                                                          response -> {
-                                                                              logger.info("proxying response: " + response.statusCode());
-                                                                              request.response().setChunked(true);
-                                                                              request.response().setStatusCode(response.statusCode());
-                                                                              request.response().headers().setAll(response.headers());
-                                                                              response.handler(data -> request.response().write(data));
-                                                                              response.endHandler((v) -> request.response().end());
-                                                                          });
-                          proxyRequest.setChunked(true);
-                          proxyRequest.headers().setAll(request.headers());
-                          request.handler(data -> proxyRequest.write(data));
-                          request.endHandler((v) -> proxyRequest.end());
-                      })
+                      .websocketHandler(this::proxyWebSocket)
+                      .requestHandler(this::proxyHttp)
                       .listen(8181);
         logger.info("start proxy server");
         startFuture.complete();
+    }
+
+    private void proxyWebSocket(ServerWebSocket ws) {
+        ws.handler(data -> {
+            logger.info("proxy message:" + data.toString("utf-8"));
+            client.websocket(8080, "localhost", "/some-uri", websocket -> {
+                websocket.handler(proxy -> {
+                    logger.info("server message:" + proxy.toString("utf-8"));
+                    ws.writeBinaryMessage(proxy);
+                });
+                websocket.writeBinaryMessage(data);
+            });
+        });
+    }
+
+    private void proxyHttp(HttpServerRequest request) {
+        logger.info("proxying request:" + request.uri());
+        HttpClientRequest proxyRequest = client.request(request.method(),
+                                                        8080,
+                                                        "localhost",
+                                                        request.uri(),
+                                                        response -> {
+                                                            logger.info("proxying response: " + response.statusCode());
+                                                            request.response().setChunked(true);
+                                                            request.response().setStatusCode(response.statusCode());
+                                                            request.response().headers().setAll(response.headers());
+                                                            response.handler(data -> request.response().write(data));
+                                                            response.endHandler((v) -> request.response().end());
+                                                        });
+        proxyRequest.setChunked(true);
+        proxyRequest.headers().setAll(request.headers());
+        request.handler(data -> proxyRequest.write(data));
+        request.endHandler((v) -> proxyRequest.end());
     }
 
     @Override
